d9044264c5b2a7b4c9ee02b935d2ae0a6b308d97
kashishInamdar <kashishinamdar2004@gmail.com>
2023-11-17 00:42:05 +0530

    for changing Device

--- index.js
+++ index.js
@@ -43,6 +43,37 @@ app.get("/products" , async (req , res)=>{
 app.post("/product" , async (req ,res)=>{
     const {name ,description , price, productImage , brand  } = req.body;
 
+    if(!name){
+        return res.json({
+            success : false,
+            message : "Name is required"
+        }) 
+    }
+    if(!description){
+        return res.json({
+            success : false,
+            message : "discription is required"
+        }) 
+    }
+    if(!price){
+        return res.json({
+            success : false,
+            message : "Price is required"
+        }) 
+    }
+    if(!productImage){
+        return res.json({
+            success : false,
+            message : "ProductImage is required"
+        }) 
+    }
+    if(!brand){
+        return res.json({
+            success : false,
+            message : "Brand is required"
+        }) 
+    }
+
     const newProduct = new Product({
         name : name ,
         description : description , 
@@ -90,6 +121,38 @@ app.put("/product/:_id" , async (req , res)=>{
     const { _id } = req.params ;
     const {name ,description , price, productImage , brand  } = req.body;
 
+    if(!name){
+        return res.json({
+            success : false,
+            message : "Name is required"
+        }) 
+    }
+    if(!description){
+        return res.json({
+            success : false,
+            message : "discription is required"
+        }) 
+    }
+    if(!price){
+        return res.json({
+            success : false,
+            message : "Price is required"
+        }) 
+    }
+    if(!productImage){
+        return res.json({
+            success : false,
+            message : "ProductImage is required"
+        }) 
+    }
+    if(!brand){
+        return res.json({
+            success : false,
+            message : "Brand is required"
+        }) 
+    }
+
+
     await Product.updateOne(
         {_id , _id},
         {$set : {
@@ -107,6 +170,38 @@ app.put("/product/:_id" , async (req , res)=>{
         success : true ,
         date : updatedProduct ,
         massage : `Successfully update student with id ${_id}`
+    })
+});
+
+app.patch("/product/:_id" , async (req , res)=>{
+    const {_id} = req.params;
+
+    const {name , description , price, productImage , brand} = req.body;
+
+    const product = await Product.findById(_id);
+
+    if(name){
+        product.name = name;
+    }
+    if(price){
+        product.price = price;
+    }
+    if(description){
+        product.description = description;
+    }
+    if(productImage){
+        product.productImage = productImage;
+    }
+    if(brand){
+        product.brand = brand;
+    }
+
+    const updatedProduct = await product.save();
+
+    res.json({
+        success : true ,
+        data : updatedProduct,
+        message : "Successfully updated",
     })
 });
 
